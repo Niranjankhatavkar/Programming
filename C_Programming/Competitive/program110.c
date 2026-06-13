381f8f52ffaf6504c750590093e989fff085ffc4
Niranjankhatavkar <niranjankhatavkar07@gmail.com>
2026-06-13 23:11:37 +0530

    Add files via upload

---
+++ C_Programming/Competitive/program110.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+void Pattern(int iNo)
+{
+    int iCnt = 0;
+    
+    for(iCnt = 2;iCnt <= (iNo*2);iCnt = iCnt + 2)
+    {
+        printf("%d\t",iCnt);   
+        
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter Number: ");
+    scanf("%d",&iValue);
+
+    Pattern(iValue);
+
+    return 0;
+}
---
+++ C_Programming/Competitive/program117.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+
+void Pattern(int iRow,int iCol)
+{
+    int i = 0;
+    int j = 0;
+    char ch = '\0';
+
+    for(i = 1; i <= iRow; i++)
+    {
+        if(i % 2 == 0)
+        {
+            for(j = 1,ch = 'a'; j <= iCol; j++,ch++)  
+            {
+                printf("%c\t",ch);
+            }
+        }
+        else
+        {
+            for(j = 1,ch = 'A'; j <= iCol; j++,ch++)  
+            {
+                printf("%c\t",ch);
+            }
+        }
+
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0;
+    int iValue2 = 0;
+
+    printf("Enter Number of Rows: ");
+    scanf("%d",&iValue1);
+
+    printf("Enter Number of Column: ");
+    scanf("%d",&iValue2);
+
+    Pattern(iValue1,iValue2);
+
+    return 0;
+}
