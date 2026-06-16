27d6b97e09aa2eb0b688260d4cb01750faa6010c
Niranjankhatavkar <niranjankhatavkar07@gmail.com>
2026-06-16 22:39:20 +0530

    Add files via upload

---
+++ C_Programming/Competitive/program140.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+void Pattern(int iRow,int iCol)
+{
+    int i = 0,j = 0,iCount = 0;
+    
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = 1,iCount = 1; j <= iCol; j++,iCount++)
+        {
+            if(i == j ||i == 1||i == iRow || j == 1 || j == iCol)
+            {
+                printf("%d\t",iCount);
+            }
+            else
+            {
+                printf("\t");
+            }
+
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0,iValue2 = 0;
+
+    printf("Enter Number of Rows : ");
+    scanf("%d",&iValue1);
+
+    printf("Enter Number of Columns : ");
+    scanf("%d",&iValue2);
+
+    Pattern(iValue1,iValue2);
+
+    return 0;
+}
