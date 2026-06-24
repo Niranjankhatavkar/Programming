9e2f4a5573d95d0e972c6261e05d9ca98c584ed1
Niranjankhatavkar <niranjankhatavkar07@gmail.com>
2026-06-24 20:31:17 +0530

    Add files via upload

---
+++ C_Programming/Practice/program247.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<string.h>
+
+void strlenX(char *str)
+{
+    *str = 'A';
+}
+
+int main()
+{
+    char Arr[50] = {'\0'};
+    int iRet = 0;
+
+    printf("Enter String : \n");
+    scanf("%[^'\n']s",Arr);    
+
+    strlenX(Arr);
+
+    printf("String is : %s\n",Arr);
+
+    return 0;
+}
