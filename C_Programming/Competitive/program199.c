2c2c55b66aba14be4ebf2c13b07a395f7ddb0185
Niranjankhatavkar <niranjankhatavkar07@gmail.com>
2026-07-15 18:14:20 +0530

    Add files via upload

---
+++ C_Programming/Competitive/program199.c
@@ -0,0 +1,274 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node* PNODE;
+typedef struct node** PPNODE;
+
+void Display(PNODE first)
+{  
+    while(first != NULL)
+    {
+        printf("| %d | -> ",first->data);
+        first = first->next;
+    }
+    printf("NULL\n");
+}
+
+int Count(PNODE first)
+{   
+    int iCount = 0;
+
+    if(first == NULL)
+    {
+        return 0;
+    }
+
+    else
+    {
+        while(first != NULL)
+        {
+            iCount++;
+            first = first->next;
+        }
+    }
+
+    return iCount;
+}
+
+void InsertFirst(PPNODE first, int iNo)
+{
+    PNODE newn = NULL;
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn->data = iNo;
+    newn->next = NULL;
+
+
+    if(*first == NULL)
+    {
+        *first = newn;
+    }
+
+    else
+    {
+        newn->next = *first;
+        *first = newn;
+    }
+}
+
+void InsertLast(PPNODE first, int iNo)
+{
+    PNODE newn = NULL;
+    PNODE temp = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn->data = iNo;
+    newn->next = NULL;
+
+
+    if(*first == NULL)
+    {
+        *first = newn;
+    }
+
+    else
+    {
+        temp = *first;
+
+        while(temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+
+        temp->next = newn;
+    }
+}
+
+void InsertAtPos(PPNODE first, int iNo, int iPos)
+{
+    PNODE newn = NULL;
+    PNODE temp = NULL;
+
+    int iCount = 0;
+    int i = 0;
+
+    iCount = Count(*first);
+
+    if(iPos < 1 || iPos > iCount+1)
+    {
+        printf("Invalid Position\n");
+        return;
+    }
+
+    if(iPos == 1)
+    {
+        InsertFirst(first,iNo);
+    }
+
+    else if(iPos == iCount + 1)
+    {
+        InsertLast(first,iNo);
+    }
+
+    else
+    {
+        newn = (PNODE)malloc(sizeof(NODE));
+
+        newn->data = iNo;
+        newn->next = NULL;
+
+        temp = *first;
+
+        for(i = 1; i < (iPos-1); i++)
+        {
+            temp = temp->next;
+        }
+
+        newn->next = temp->next;
+        temp->next = newn;
+    }
+}
+
+void DeleteFirst(PPNODE first)
+{
+    PNODE temp = NULL;
+
+    if(*first == NULL)
+    {
+        return;
+    }
+
+    else if((*first)->next == NULL)
+    {
+        free(*first);
+        *first = NULL;
+    }
+
+    else
+    {
+        temp = *first;
+
+        *first = (*first)->next;
+
+        free(temp);
+    }
+}
+
+void DeleteLast(PPNODE first)
+{   
+    PNODE temp = NULL;
+
+    if(*first == NULL)
+    {
+        return;
+    }
+
+    else if((*first)->next == NULL)
+    {
+        free(*first);
+        *first = NULL;
+    }
+
+    else
+    {
+        temp = *first;
+
+        while(temp->next->next != NULL)
+        {
+            temp = temp->next;
+        }
+
+        free(temp->next);
+
+        temp->next = NULL;
+    }
+}
+
+void DeleteAtPos(PPNODE first, int iPos)
+{
+    PNODE temp = NULL;
+    PNODE target = NULL;
+
+    int iCount = 0;
+    int i = 0;
+
+    iCount = Count(*first);
+
+    if(iPos < 1 || iPos > iCount)
+    {
+        printf("Invalid Position\n");
+        return;
+    }
+
+    if(iPos == 1)
+    {
+        DeleteFirst(first);
+    }
+
+    else if(iPos == iCount)
+    {
+        DeleteLast(first);
+    }
+
+    else
+    {
+        temp = *first;
+
+        for(i = 1; i < (iPos-1); i++)
+        {
+            temp = temp->next;
+        }
+
+        target = temp->next;
+
+        temp->next = target->next;
+
+        free(target);
+    }
+}
+
+int CountGreater(PNODE first,int iNo)
+{       
+    int iCount = 0;
+
+    while(first != NULL)
+    {
+        if(first->data > iNo)
+        {
+            iCount++;
+        }        
+
+        first = first->next;
+    }
+
+    return iCount;
+}
+
+int main()
+{
+    PNODE head = NULL;
+
+    int iRet = 0;
+
+    InsertFirst(&head, 51);
+    InsertFirst(&head, 21);
+    InsertFirst(&head, 11);
+
+    InsertLast(&head,110);
+    InsertLast(&head,122);
+    InsertLast(&head,214);
+
+    iRet = CountGreater(head,21);
+
+    printf("Greater Numbers : %d\n",iRet);
+
+    return 0;
+}
